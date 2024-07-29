88f09842c77244a6b613164e47daafa7d8c1fb33
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2024-07-29 14:24:46 +0530

    add scvs scorer package
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

---
+++ pkg/scorer/scvsScore.go
@@ -0,0 +1,50 @@
+// Copyright 2023 Interlynk.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scorer
+
+type ScvsScore interface {
+	Category() string
+	Feature() string
+	Score() string
+}
+
+type scvsScore struct {
+	category string
+	feature  string
+	score    string
+}
+
+func newScoreFromScvsCheck(c *scvsCheck) *scvsScore {
+	return &scvsScore{
+		category: c.Category,
+		feature:  c.Key,
+	}
+}
+
+func (s *scvsScore) setScore(v string) {
+	s.score = v
+}
+
+func (s scvsScore) Category() string {
+	return s.category
+}
+
+func (s scvsScore) Feature() string {
+	return s.feature
+}
+
+func (s scvsScore) Score() string {
+	return s.score
+}
---
+++ pkg/scorer/scvsScores.go
@@ -0,0 +1,53 @@
+// Copyright 2023 Interlynk.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scorer
+
+type ScvsScores interface {
+	Count() int
+	AvgScore() float64
+	ScoreList() []ScvsScore
+}
+
+type scvsScores struct {
+	scs []ScvsScore
+}
+
+func newScvsScores() *scvsScores {
+	return &scvsScores{
+		scs: []ScvsScore{},
+	}
+}
+
+func (s *scvsScores) addScore(ss scvsScore) {
+	s.scs = append(s.scs, ss)
+}
+
+func (s scvsScores) Count() int {
+	return len(s.scs)
+}
+
+func (s scvsScores) AvgScore() float64 {
+	score := 0.0
+	for _, s := range s.scs {
+		if s.Score() == "✓" {
+			score++
+		}
+	}
+	return score / float64(s.Count())
+}
+
+func (s scvsScores) ScoreList() []ScvsScore {
+	return s.scs
+}
