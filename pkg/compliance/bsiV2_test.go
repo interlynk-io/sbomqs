fe4cf665f046f9c13f0645f1c5d001c3ee712242
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2024-11-28 20:32:15 +0530

    add bsi2 test cases for spdx and cdx bomlinks
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- pkg/compliance/bsiV2_test.go
+++ pkg/compliance/bsiV2_test.go
@@ -3,6 +3,7 @@ package compliance
 import (
 	"testing"
 
+	"github.com/interlynk-io/sbomqs/pkg/compliance/common"
 	db "github.com/interlynk-io/sbomqs/pkg/compliance/db"
 	"github.com/interlynk-io/sbomqs/pkg/sbom"
 	"gotest.tools/assert"
@@ -105,6 +106,165 @@ func TestBSIV2CDXSbomVulnerability(t *testing.T) {
 				score:  0.0,
 				result: "CVE-2018-7489, CVE-2021-44228",
 				key:    SBOM_VULNERABILITES,
+				id:     "doc",
+			},
+		},
+	}
+	for _, test := range testCases {
+		assert.Equal(t, test.expected.score, test.actual.Score, "Score mismatch for %s", test.name)
+		assert.Equal(t, test.expected.key, test.actual.CheckKey, "Key mismatch for %s", test.name)
+		assert.Equal(t, test.expected.id, test.actual.ID, "ID mismatch for %s", test.name)
+		assert.Equal(t, test.expected.result, test.actual.CheckValue, "Result mismatch for %s", test.name)
+	}
+}
+
+func spdxDocWithNoExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+
+	s.ExternalDocReference = nil
+	doc := sbom.SpdxDoc{
+		SpdxSpec: s,
+	}
+	return doc
+}
+
+func spdxDocWithOneExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+	spdxDocument := []string{"https://example.com/spdx/docs/toolsetX-v1.2"}
+	s.ExternalDocReference = spdxDocument
+	doc := sbom.SpdxDoc{
+		SpdxSpec: s,
+	}
+	return doc
+}
+
+func spdxDocWithMultipleExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+	spdxDocument := []string{"https://interlynk.io/github.com%2Finterlynk-io%2Fsbomqs/0.0.15/qIP32aoJi0u5M_EjHeJHAg", "https://example.com/spdx/docs/toolsetX-v1.2"}
+	s.ExternalDocReference = spdxDocument
+	doc := sbom.SpdxDoc{
+		SpdxSpec: s,
+	}
+	return doc
+}
+
+func TestBSIV2SPDXSbomBomLinks(t *testing.T) {
+	value := "https://interlynk.io/github.com%2Finterlynk-io%2Fsbomqs/0.0.15/qIP32aoJi0u5M_EjHeJHAg, https://example.com/spdx/docs/toolsetX-v1.2"
+	wrappedURL := common.WrapText(value, 80)
+
+	testCases := []struct {
+		name     string
+		actual   *db.Record
+		expected desired
+	}{
+		{
+			name:   "SPDX SBOM with no bom links",
+			actual: bsiV2SbomLinks(spdxDocWithNoExtDocumentRefs()),
+			expected: desired{
+				score:  0.0,
+				result: "",
+				key:    SBOM_BOM_LINKS,
+				id:     "doc",
+			},
+		},
+		{
+			name:   "SPDX SBOM with one bom links",
+			actual: bsiV2SbomLinks(spdxDocWithOneExtDocumentRefs()),
+			expected: desired{
+				score:  10.0,
+				result: "https://example.com/spdx/docs/toolsetX-v1.2",
+				key:    SBOM_BOM_LINKS,
+				id:     "doc",
+			},
+		},
+		{
+			name:   "SPDX SBOM with two bom links vulnerability",
+			actual: bsiV2SbomLinks(spdxDocWithMultipleExtDocumentRefs()),
+			expected: desired{
+				score:  10.0,
+				result: wrappedURL,
+				key:    SBOM_BOM_LINKS,
+				id:     "doc",
+			},
+		},
+	}
+	for _, test := range testCases {
+		assert.Equal(t, test.expected.score, test.actual.Score, "Score mismatch for %s", test.name)
+		assert.Equal(t, test.expected.key, test.actual.CheckKey, "Key mismatch for %s", test.name)
+		assert.Equal(t, test.expected.id, test.actual.ID, "ID mismatch for %s", test.name)
+		assert.Equal(t, test.expected.result, test.actual.CheckValue, "Result mismatch for %s", test.name)
+	}
+}
+
+func cdxDocWithNoExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+
+	s.ExternalDocReference = nil
+	doc := sbom.CdxDoc{
+		CdxSpec: s,
+	}
+	return doc
+}
+
+func cxDocWithOneExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+	extRefs := []string{"https://raw.githubusercontent.com/bomctl/bomctl-playground/main/examples/bomctl-container-image/app/bomctl_0.3.0_linux_amd64.tar.gz.spdx.json"}
+	s.ExternalDocReference = extRefs
+	doc := sbom.SpdxDoc{
+		SpdxSpec: s,
+	}
+	return doc
+}
+
+func cxDocWithMultipleExtDocumentRefs() sbom.Document {
+	s := sbom.NewSpec()
+	extRefs := []string{"https://raw.githubusercontent.com/bomctl/bomctl-playground/main/examples/bomctl-container-image/app/bomctl_0.3.0_linux_amd64.tar.gz.spdx.json", "https://interlynk.io/github.com%2Finterlynk-io%2Fsbomqs/0.0.15/qIP32aoJi0u5M_EjHeJHAg"}
+	s.ExternalDocReference = extRefs
+	doc := sbom.SpdxDoc{
+		SpdxSpec: s,
+	}
+	return doc
+}
+
+func TestBSIV2CDXSbomBomLinks(t *testing.T) {
+	value := "https://raw.githubusercontent.com/bomctl/bomctl-playground/main/examples/bomctl-container-image/app/bomctl_0.3.0_linux_amd64.tar.gz.spdx.json"
+	wrappedURL := common.WrapText(value, 80)
+
+	value2 := "https://raw.githubusercontent.com/bomctl/bomctl-playground/main/examples/bomctl-container-image/app/bomctl_0.3.0_linux_amd64.tar.gz.spdx.json, https://interlynk.io/github.com%2Finterlynk-io%2Fsbomqs/0.0.15/qIP32aoJi0u5M_EjHeJHAg"
+	wrappedURL2 := common.WrapText(value2, 80)
+
+	testCases := []struct {
+		name     string
+		actual   *db.Record
+		expected desired
+	}{
+		{
+			name:   "CDX SBOM with no bom links",
+			actual: bsiV2SbomLinks(cdxDocWithNoExtDocumentRefs()),
+			expected: desired{
+				score:  0.0,
+				result: "",
+				key:    SBOM_BOM_LINKS,
+				id:     "doc",
+			},
+		},
+		{
+			name:   "CDX SBOM with one bom links",
+			actual: bsiV2SbomLinks(cxDocWithOneExtDocumentRefs()),
+			expected: desired{
+				score:  10.0,
+				result: wrappedURL,
+				key:    SBOM_BOM_LINKS,
+				id:     "doc",
+			},
+		},
+		{
+			name:   "CDX SBOM with one bom links",
+			actual: bsiV2SbomLinks(cxDocWithMultipleExtDocumentRefs()),
+			expected: desired{
+				score:  10.0,
+				result: wrappedURL2,
+				key:    SBOM_BOM_LINKS,
 				id:     "doc",
 			},
 		},
