6e52e27db5abc4b8621dc833cf656994be3dba81
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2025-12-19 14:13:12 +0530

    fix path issue

--- pkg/validation/schema.go
+++ pkg/validation/schema.go
@@ -14,7 +14,8 @@ var schemaFS embed.FS
 func schemaPath(spec, version string) string {
 	switch spec {
 	case "cyclonedx":
-		return fmt.Sprintf("pkg/validation/schemas/cyclonedx/%s/bom-%s.schema.json", version, version)
+		return fmt.Sprintf("http://cyclonedx.org/schema/bom-%s.schema.json", version)
+		// return fmt.Sprintf("pkg/validation/schemas/cyclonedx/%s/bom-%s.schema.json", version, version)
 	default:
 		return ""
 	}
@@ -25,27 +26,28 @@ var schemaRegistry = map[string]string{
 	"http://cyclonedx.org/schema/spdx.schema.json":     "schemas/cyclonedx/common/spdx.schema.json",
 	"http://cyclonedx.org/schema/jsf-0.82.schema.json": "schemas/cyclonedx/common/jsf-0.82.schema.json",
 
-	// Root schemas (add more versions as needed)
+	// Root schemas
 	"http://cyclonedx.org/schema/bom-1.2.schema.json": "schemas/cyclonedx/1.2/bom-1.2.schema.json",
 	"http://cyclonedx.org/schema/bom-1.3.schema.json": "schemas/cyclonedx/1.3/bom-1.3.schema.json",
 	"http://cyclonedx.org/schema/bom-1.4.schema.json": "schemas/cyclonedx/1.4/bom-1.4.schema.json",
 	"http://cyclonedx.org/schema/bom-1.5.schema.json": "schemas/cyclonedx/1.5/bom-1.5.schema.json",
+	"http://cyclonedx.org/schema/bom-1.6.schema.json": "schemas/cyclonedx/1.6/bom-1.6.schema.json",
+	"http://cyclonedx.org/schema/bom-1.7.schema.json": "schemas/cyclonedx/1.7/bom-1.7.schema.json",
 }
 
-func loadSchema(schemaPath string) (*jsonschema.Schema, error) {
-	if schemaPath == "" {
-		return nil, fmt.Errorf("unknown schema path: %s", schemaPath)
+func loadSchema(schemaURL string) (*jsonschema.Schema, error) {
+	if schemaURL == "" {
+		return nil, fmt.Errorf("unknown schema")
 	}
 
 	c := jsonschema.NewCompiler()
 
-	// 1. Preload all known schemas
 	if err := preloadSchemas(c); err != nil {
 		return nil, err
 	}
 
-	// 2. Compile root schema by embedded path
-	return c.Compile(schemaPath)
+	// Compile by canonical $id (URL)
+	return c.Compile(schemaURL)
 }
 
 func preloadSchemas(c *jsonschema.Compiler) error {
